947bf292d0096901af88272afb39f4532b0e1c6c
1g2g <87280835+1g2g@users.noreply.github.com>
2021-09-01 21:12:59 +0900

    Delete 210810 수정 directory

--- 210810 수정/javascript/login.js
+++
@@ -1,5 +0,0 @@
-function empty_value_test_login(){//비어있는 값에 대한 검사
-  if(document.querySelector('#userid').value=="") alert('아이디를 입력하세요.');
-  else if(document.querySelector('#password').value=="") alert('비밀번호를 입력하세요.');
-
-}
