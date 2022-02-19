cb954f5803cc879bc13aa293fce3b3691ba2c786
1g2g <87280835+1g2g@users.noreply.github.com>
2022-02-19 16:11:29 +0900

    Update home.js

--- javascript/home.js
+++ javascript/home.js
@@ -11,9 +11,6 @@ document.querySelector('.parallax_page').addEventListener('scroll', function () 
         nav.style.position = "fixed";
         nav.style.right = "calc((101% - 1162px)/2)";
         nav.style.bottom = "0";
-        if (scrollLocation >= 1550) {
-            nav.style.bottom = "60px";//1522+1588/2, footer높이 60
-        }
         //  alert('3');
     }
     //content의 너비보다 창이 좁으면cont_right가 스크롤 이동에 따라 움직임
@@ -21,11 +18,12 @@ document.querySelector('.parallax_page').addEventListener('scroll', function () 
         nav.style.position = "fixed";
         nav.style.left = "900px";
         nav.style.bottom = "0";
-        if (scrollLocation >= 1550) nav.style.bottom = "60px";
     }
     //상단으로 올라가거나 모바일 버전이면 원래 위치로 이동
     else {
         nav.style.position = "static";
-
+    }
+    if (scrollLocation >= 1550) {
+        nav.style.bottom = "60px";//1522+1588/2, footer높이 60
     }
 });
