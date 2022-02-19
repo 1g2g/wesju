07dd26ebe0f8373eeae99f03ee3beec3be20d02c
1g2g <87280835+1g2g@users.noreply.github.com>
2022-02-19 14:09:49 +0900

    Update editor.js

--- javascript/editor.js
+++ javascript/editor.js
@@ -1,25 +1,7 @@
-$(document).ready(function() {
-  $('#summernote').summernote({ 
-        height: 300,                 // 에디터 높이
-        minHeight: null,             // 최소 높이
-        maxHeight: null,             // 최대 높이
-        focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
-        lang: "ko-KR",					// 한글 설정
-        placeholder: '욕이나 비방글은 예고없이 삭제될 수 있습니다.',	//placeholder 설정
-        toolbar: [
-            // [groupName, [list of button]]
-            ['fontname', ['fontname']],
-            ['fontsize', ['fontsize']],
-            ['style', ['bold', 'italic', 'underline','strikethrough', 'clear']],
-            ['color', ['forecolor','color']],
-            ['table', ['table']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['height', ['height']],
-            ['insert',['picture','link','video']],
-            ['view', ['fullscreen', 'help']]
-          ],
-        fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체'],
-        fontSizes: ['8','9','10','11','12','14','16','18','20','22','24','28','30','36','50','72']
-
-    });
-});
+function rating_limit() {
+    var num = document.querySelector('.rating_star').value;
+    console.log(num);
+    if (num < 0||num > 4.5) {
+        alert("0과 4.5사이의 숫자를 입력해주세요");
+    }
+}
